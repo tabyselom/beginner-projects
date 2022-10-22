40a43ad2076b149bced01109d3ccc3047505932b
tabyselom <abyselom111@gmail.com>
2022-10-22 12:54:49 +0300

    projects on numbers

---
+++ Numbers/e to the Nth Digit.py
@@ -0,0 +1,38 @@
+'''
+name = pi rounding
+helps to round pi number to the decimal place we want
+'''
+
+
+# by importing mathe we can get the actual value of pi
+import math
+
+pi = math.e
+#we take the lengthe of decimal that the pi would have
+theNumberOfDecimal=int(input("How many decimal place of pi: "))
+'''
+name = pi rounding
+helps to round pi number to the decimal place we want
+'''
+
+
+# by importing mathe we can get the actual value of pi
+import math
+
+pi = math.pi
+#we take the lengthe of decimal that the pi would have
+theNumberOfDecimal=int(input("How many decimal place of pi: "))
+
+#the round take two argument 
+#the frst the number we will round
+#the secod the decimal it will be rouned to
+ans =round(pi,theNumberOfDecimal)
+
+print(ans)
+
+#the round take two argument 
+#the frst the number we will round
+#the secod the decimal it will be rouned to
+ans =round(pi,theNumberOfDecimal)
+
+print(ans)
