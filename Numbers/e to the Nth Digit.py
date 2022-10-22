0c828b12519ad6302b25cc2e93b7db95bc10c419
tabyselom <abyselom111@gmail.com>
2022-10-22 12:58:28 +0300

    rounding th decimal point of e

--- Numbers/e to the Nth Digit.py
+++ Numbers/e to the Nth Digit.py
@@ -1,38 +1,18 @@
 '''
-name = pi rounding
-helps to round pi number to the decimal place we want
+name = e rounding
+helps to round e number to the decimal place we want
 '''
 
 
-# by importing mathe we can get the actual value of pi
+# by importing mathe we can get the actual value of e
 import math
 
-pi = math.e
-#we take the lengthe of decimal that the pi would have
-theNumberOfDecimal=int(input("How many decimal place of pi: "))
-'''
-name = pi rounding
-helps to round pi number to the decimal place we want
-'''
-
-
-# by importing mathe we can get the actual value of pi
-import math
-
-pi = math.pi
-#we take the lengthe of decimal that the pi would have
-theNumberOfDecimal=int(input("How many decimal place of pi: "))
-
+e = math.e
+#we take the lengthe of decimal that the e would have
+theNumberOfDecimal=int(input("How many decimal place of e: "))
 #the round take two argument 
 #the frst the number we will round
 #the secod the decimal it will be rouned to
-ans =round(pi,theNumberOfDecimal)
-
-print(ans)
-
-#the round take two argument 
-#the frst the number we will round
-#the secod the decimal it will be rouned to
-ans =round(pi,theNumberOfDecimal)
+ans =round(e,theNumberOfDecimal)
 
 print(ans)
