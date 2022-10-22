e5b739b0be369e6a36717cce74a2bfae5a1475b0
tabyselom <abyselom111@gmail.com>
2022-10-22 12:45:00 +0300

    projects on numbers

---
+++ Numbers/Find_PI_to_the_Nth_Digit.py
@@ -0,0 +1,19 @@
+'''
+name = pi rounding
+helps to round pi number to the decimal place we want
+'''
+
+
+# by importing mathe we can get the actual value of pi
+import math
+
+pi = math.pi
+#we take the lengthe of decimal that the pi would have
+theNumberOfDecimal=int(input("How many decimal place of pi: "))
+
+#the round take two argument 
+#the frst the number we will round
+#the secod the decimal it will be rouned to
+ans =round(pi,theNumberOfDecimal)
+
+print(ans)
